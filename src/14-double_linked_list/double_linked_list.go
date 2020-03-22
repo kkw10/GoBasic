691cd0579d6fea74e4c01f95fd8e032ad8a863f6
kkw_10 <kkw_10@naver.com>
2020-03-22 13:15:21 +0900

    14-double-linked-list

---
+++ src/14-double_linked_list/double_linked_list.go
@@ -0,0 +1,96 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	next *Node
+	prev *Node
+	val int
+}
+
+type LinkedList struct {
+	root *Node
+	tail *Node
+}
+
+func (l *LinkedList) AddNode(val int) {
+	if l.root == nil {
+		l.root = &Node{val: val}
+		l.tail = l.root
+		return
+	}
+
+	l.tail.next = &Node{val: val}
+	prev := l.tail
+	l.tail = l.tail.next
+	l.tail.prev = prev
+}
+
+func (l *LinkedList) RemoveNode(node *Node) {
+	if node == l.root {
+		l.root = l.root.next
+		l.root.prev = nil
+		node.next = nil
+		return
+	}
+
+	prev := node.prev
+
+	if node == l.tail {
+		prev.next = nil
+		l.tail.prev = nil
+		l.tail = prev
+	} else {
+		node.prev = nil
+		prev.next = prev.next.next
+		prev.next.prev = prev
+	}
+
+	node.next = nil
+}
+
+func (l *LinkedList) PrintNodes() {
+	node := l.root
+	for node.next != nil {
+		fmt.Printf("%d -> ", node.val)
+		node = node.next
+	}
+
+	fmt.Printf("%d\n", node.val)
+}
+
+func (l *LinkedList) PrintRevers() {
+	node := l.tail
+
+	for node.prev != nil {
+		fmt.Printf("%d -> ", node.val)
+		node = node.prev
+	}
+
+	fmt.Printf("%d\n", node.val)
+}
+
+func main() {
+	list := &LinkedList{}
+	list.AddNode(0)
+
+	for i := 1; i < 10; i++ {
+		list.AddNode(i)
+	}
+
+	list.PrintNodes()
+
+	list.RemoveNode(list.root.next)
+
+	list.PrintNodes()
+
+	list.RemoveNode(list.root)
+
+	list.PrintNodes()
+
+	list.RemoveNode(list.tail)
+
+	list.PrintNodes()
+
+	list.PrintRevers()
+}
